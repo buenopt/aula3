90d69d8f377f78798bf1e064d7f1d44431d204ca
Fernando Bueno <BUENOPT@HOTMAIL.COM>
2023-06-11 16:18:12 -0300

    Exercicio 1 aula 3:
    Implementar 2 casos de teste em JUnit para o método “existePessoa()”:
    1 - Deve testar a situação no qual a pessoa existe;
    2 - Situação no qual a pessoa não existe.
    Use o Mockito para simular o retorno do método “getAllPessoas()”.

---
+++ src/main/java/bueno/fernando/aula3/Aula3.java
@@ -0,0 +1,13 @@
+package bueno.fernando.aula3;
+
+/**
+ *
+ * @author fbueno
+ */
+public class Aula3 {
+
+    public static void main(String[] args) {
+        System.out.println("Exercicio 1");
+
+    }
+}
---
+++ src/main/java/bueno/fernando/aula3/RHService.java
@@ -0,0 +1,13 @@
+package bueno.fernando.aula3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fbueno
+ */
+public interface RHService {
+
+    public ArrayList<Pessoa> getAllPessoas();
+
+}
